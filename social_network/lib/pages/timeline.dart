908aa3058a95f64f8e4f16aa1f7639d79bc5ba9b
SirJulian <secret@333>
2019-09-26 16:24:54 -0300

    Inicio Aula 10

--- social_network/lib/pages/timeline.dart
+++ social_network/lib/pages/timeline.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection("users");
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,10 +12,38 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  // getUserById() async {
+  //   final DocumentSnapshot doc =
+  //       await usersRef.document("ZGwKpJy9fRXXCjO3qLMr").get();
+
+  //   print(doc.data["username"]);
+  // }
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef
+        .limit(1)
+        .orderBy("postsCount", descending: true)
+        .getDocuments();
+    snapshot.documents.forEach((DocumentSnapshot doc) {
+      print(doc.data);
+      print(doc.documentID);
+      print(doc.exists);
+    });
+
+    // usersRef.getDocuments().then((QuerySnapshot snapshot) {
+    //
+    // });
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text("Timeline"),
+      body: linearProgress(),
     );
   }
 }
